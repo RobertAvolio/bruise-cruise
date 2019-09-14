28be826f0772d0c58e39f22fab51510b00895d95
Ben Przysucha <ben.mozilla@gmail.com>
2019-09-13 20:52:51 -0400

    Added a temporary player and made a movement only system

---
+++ Bruise Cruise/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed;
+
+    private Rigidbody2D rb2d;
+    private Vector2 moveVector;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), 0f);
+        moveVector = moveInput.normalized * speed;
+    }
+
+    void FixedUpdate ()
+    {
+        rb2d.MovePosition(rb2d.position + moveVector * Time.fixedDeltaTime);
+    }
+}
