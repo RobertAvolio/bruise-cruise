07abd85dcc7b54c8aae0f1eafaee6ab44aef1b57
Robert Avolio <ravolio1@umbc.edu>
2019-09-13 22:49:40 -0400

    BC-004: Combat System
    Added combat. Used hitboxes attached to the player that activate when a button is pressed. Added a combo system to do different attacks in succession. Added an enemy with health.

---
+++ Bruise Cruise/Assets/Scripts/Enemy.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public int health = 5;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(health <= 0)
+        {
+            this.gameObject.SetActive(false);
+        }
+    }
+
+    public void TakeDamage()
+    {
+        health--;
+        Debug.Log("Take Damage called.");
+    }
+}
---
+++ Bruise Cruise/Assets/Scripts/PlayerInputTimer.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInputTimer : MonoBehaviour
+{
+    public Attack attack1;
+    public Attack attack2;
+    public Attack attack3;
+    private Renderer rend1;
+    private Renderer rend2;
+    private Renderer rend3;
+    private BoxCollider2D box1;
+    private BoxCollider2D box2;
+    private BoxCollider2D box3;
+    private float timer;
+    private bool hit2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rend1 = attack1.GetComponent<Renderer>();
+        rend2 = attack2.GetComponent<Renderer>();
+        rend3 = attack3.GetComponent<Renderer>();
+        box1 = attack1.GetComponent<BoxCollider2D>();
+        box2 = attack2.GetComponent<BoxCollider2D>();
+        box3 = attack3.GetComponent<BoxCollider2D>();
+        timer = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        rend1.enabled = false;
+        box1.enabled = false;
+        rend2.enabled = false;
+        box2.enabled = false;
+        rend3.enabled = false;
+        box3.enabled = false;
+        if (Input.GetKeyUp("e"))
+        {
+            if(timer <= 0)
+            {
+                rend1.enabled = true;
+                box1.enabled = true;
+                timer = 3;
+            } else if(!hit2)
+            {
+                rend2.enabled = true;
+                box2.enabled = true;
+                hit2 = true;
+            }
+            else
+            {
+                rend3.enabled = true;
+                box3.enabled = true;
+                timer = 0;
+            }
+            
+            
+        }
+        if(timer > 0)
+        {
+            timer -= Time.deltaTime;
+        } else
+        {
+            hit2 = false;
+        }
+
+    }
+}
