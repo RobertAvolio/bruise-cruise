2a776241a1053dc8b4a06d78b8efb339b7bac7bc
JLoiXP <justinhloi@yahoo.com>
2019-09-14 17:16:29 -0400

    BC-002
    Working health bar with text

---
+++ Bruise Cruise/Assets/Scenes/Scripts/HealthBar.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+    public Slider healthBar;
+    public int health = 100;
+    public Text healthText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        healthBar.value = 1;
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        healthText.text = "Health: " + health;
+        if (Input.GetKeyDown(KeyCode.O))
+        {
+            if (health > 0)
+            {
+                health-=10;
+                healthBar.value = (float)health / 100;
+
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            if (health < 100)
+            {
+                health+=10;
+                healthBar.value = (float)health / 100;
+            }
+        }
+    }
+}
