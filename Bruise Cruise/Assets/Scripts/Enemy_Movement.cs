2de406d4a0421f20c445f9bf282e516d2217eb51
tanghuynh0214 <tanghuynh0214@gmail.com>
2019-09-18 12:28:05 -0400

    Perform some movement on enemy towards player until a certain distance.

---
+++ Bruise Cruise/Assets/Scripts/Enemy_Movement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_Movement : MonoBehaviour
+{
+    public float speed;
+    public float stoppingDistance;
+    public float distance;
+    private Transform target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+      target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    // Method for enemy to move from his position to targets position
+    void Update()
+    {
+      //Keep moving towards the target until the distance <= 3
+      distance = Mathf.Abs(Vector2.Distance(transform.position, target.position));
+      if(distance > stoppingDistance)
+      {
+        transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+      }
+    }
+}
