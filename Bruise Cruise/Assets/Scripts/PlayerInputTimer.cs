4d6ff691d972dc6b79c286e90e2b6e5268210652
Ben Przysucha <ben.mozilla@gmail.com>
2019-09-18 21:47:08 -0400

    Tried to add animations (didnt work...) but added the PlayerStats script

--- Bruise Cruise/Assets/Scripts/PlayerInputTimer.cs
+++ Bruise Cruise/Assets/Scripts/PlayerInputTimer.cs
@@ -9,6 +9,7 @@ public class PlayerInputTimer : MonoBehaviour
     public float timerStart;
     private float timer;
     private bool hit2;
+    
     // Start is called before the first frame update
     void Start()
     {
