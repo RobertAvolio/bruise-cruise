77c79ea09d38a7a6f8c881b4dce03b7de459dccb
JLoiXP <justinhloi@yahoo.com>
2019-09-19 14:39:06 -0400

    BC-002 Health Bar
    Working health bar

--- Bruise Cruise/Assets/Scenes/Scripts/HealthBar.cs
+++ Bruise Cruise/Assets/Scenes/Scripts/HealthBar.cs
@@ -18,20 +18,28 @@ public class HealthBar : MonoBehaviour
     void Update()
     {
         healthText.text = "Health: " + health;
-        if (Input.GetKeyDown(KeyCode.O))
+        if (Input.GetKeyDown(KeyCode.O)) //Replace input as needed. Duplicate this if multiple damage types exist
         {
             if (health > 0)
             {
-                health-=10;
+                health-=10; //change int according to amount of dmg supposed to be done
+                if(health<0)
+                {
+                    health = 0;
+                }
                 healthBar.value = (float)health / 100;
 
             }
         }
-        if (Input.GetKeyDown(KeyCode.P))
+        if (Input.GetKeyDown(KeyCode.P)) //Replace input as needed. Duplicate this is multiple healing types exist
         {
             if (health < 100)
             {
-                health+=10;
+                health+=15; //change int according to amount of healing supposed to be done
+                if(health>100)
+                {
+                    health = 100;
+                }
                 healthBar.value = (float)health / 100;
             }
         }
