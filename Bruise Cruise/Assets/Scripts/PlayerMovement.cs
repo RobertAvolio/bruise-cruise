3d11d094db4314bc4270b7b0ef4d27ae8c464443
Ben Przysucha <ben.mozilla@gmail.com>
2019-10-04 16:57:55 -0400

    BC-019 Fixed CAMERAgit add . and added a more imporoved jump that we can mess with over time

--- Bruise Cruise/Assets/Scripts/PlayerMovement.cs
+++ Bruise Cruise/Assets/Scripts/PlayerMovement.cs
@@ -4,32 +4,40 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float speed;
-
-    private Rigidbody2D rb2d;
-    private Vector2 moveVector;
+    [SerializeField] private float speed;
     [SerializeField] private float jumpForce;
 
-    // Start is called before the first frame update
+    private float fallMultiplier = 10f;
+    private float lowJumpMultiplier = 5f;
+    private Rigidbody2D rb2d;
+    private Vector2 moveVector;
+    private bool wantsToJump = false;
+    
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        Vector2 moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), 0f);
-        moveVector = moveInput.normalized * speed;
-
-        if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown("joystick button 0")) {
-            print("Jumping!");
-            rb2d.velocity = Vector2.up * jumpForce * Time.deltaTime;
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            wantsToJump = true;
         }
     }
 
-    void FixedUpdate ()
-    {
-        rb2d.MovePosition(rb2d.position + moveVector * Time.fixedDeltaTime);
+    void FixedUpdate() {
+        rb2d.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * speed, rb2d.velocity.y);
+
+        if (wantsToJump) {
+            rb2d.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            wantsToJump = false;
+        }
+
+        if (rb2d.velocity.y < 0) {
+            rb2d.gravityScale = fallMultiplier;
+        } else if (rb2d.velocity.y > 0 && !Input.GetKey(KeyCode.Space)) {
+            rb2d.gravityScale = lowJumpMultiplier;
+        } else {
+            rb2d.gravityScale = 1f;
+        }
     }
 }
---
+++ Bruise Cruise/Assets/Scripts/RetroCamera.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class RetroCamera : MonoBehaviour
+{
+    [SerializeField] private Material effectMaterial;
+
+    void OnRenderImage(RenderTexture src, RenderTexture dst) {
+        Graphics.Blit(src,dst,effectMaterial);
+    }
+}
