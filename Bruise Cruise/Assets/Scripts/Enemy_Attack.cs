f8b8cc2235359be95af186ff2f89447700e8873b
tanghuynh0214 <tanghuynh0214@gmail.com>
2019-10-11 02:09:10 -0400

    BC-029: Leverage switch cases to transition from one state to another. Currently have patrol, chase, and attack states in the state machine.

--- Bruise Cruise/Assets/Scripts/Enemy_Attack.cs
+++ Bruise Cruise/Assets/Scripts/Enemy_Attack.cs
@@ -1,31 +1,39 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+/*
+  This class essentially allows the AI to attack the target when in radius.
+*/
 
 public class Enemy_Attack : MonoBehaviour
 {
     //Declarations
     private float waitTime = 2.0f;
     private float timer = 0.0f;
-    protected float distance;
+    public float distance;
     private Enemy_Movement obj;
+    private bool isAttacking = false;
 
     // Start is called before the first frame update
     void Start()
     {
-      Debug.Log("Welcome to Bruise-Cruise");
-      obj = GetComponent<Enemy_Movement>();
+      // Debug.Log("Welcome to Bruise-Cruise");
+      // obj = GetComponent<Enemy_Movement>();
     }
 
     // Update is called once per frame
     void Update()
+    {
+      //Does nothing now
+    }
+    void AttackPlayer()
     {
       distance = obj.getDistance();
 
       // Found the target and is some radius away from it
       if(distance <= obj.stoppingDistance)
       {
+        isAttacking = true;
         Debug.Log("target found");
         //AI is next to player and can now start the timer
         timer += Time.deltaTime;
@@ -38,9 +46,10 @@ public class Enemy_Attack : MonoBehaviour
           Attack();
           timer = 0.0f;
         }
+        isAttacking = false;
       }
     }
-    //Function to display attack message to the console
+    // Function to display attack message to the console
     void FakeAttack()
     {
       Debug.Log("Attacking the Player!");
@@ -51,5 +60,4 @@ public class Enemy_Attack : MonoBehaviour
     {
       Debug.Log("Attacking in progress..");
     }
-
 }
