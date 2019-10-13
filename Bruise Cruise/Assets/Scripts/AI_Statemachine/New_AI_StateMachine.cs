dbc9663346e397e79b099aa2c912e8e4dcf4725e
TheLoveDoctor <50722677+TheLoveDoctor@users.noreply.github.com>
2019-10-13 00:26:38 -0400

    BC-033
    Started on another state machine implementation in the AI_statemachine folder, doesn't even work a little bit

---
+++ Bruise Cruise/Assets/Scripts/AI_Statemachine/New_AI_StateMachine.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using StateMachineStuff;
+
+public class New_AI_StateMachine
+{
+    public class EnemySM : MidState
+    {
+        public override string StateUpdate()
+        {
+
+            updateState(CurrentSubState.StateUpdate());
+
+            return null;
+        }
+    }
+
+    EnemySM EnemySM_topstate = new EnemySM();
+
+    EnemySM_topstate.updateState();
+}
+
+
---
+++ Bruise Cruise/Assets/Scripts/AI_Statemachine/StateMachines.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+
+namespace StateMachineStuff
+{
+    public class State
+    {
+        public virtual void StateEnter() { }
+        public virtual string StateUpdate() => null;
+        public virtual void StateExit() { }
+    }
+
+    public class MidState : State
+    {
+
+        protected IDictionary<string, State> SubStatesMap;
+
+        protected State CurrentSubState;
+
+        public void addSubState(string name, State state)
+        {
+            SubStatesMap.Add(new KeyValuePair<string, State>(name, state));
+        }
+        protected void updateState(string NewState)
+        {
+            if (NewState != null)
+            {
+                if (SubStatesMap[NewState] != CurrentSubState)
+                {
+                    CurrentSubState.StateExit();
+                    CurrentSubState = SubStatesMap[NewState];
+                    CurrentSubState.StateEnter();
+                }
+            }
+        }
+    }
+
+    public class BottomState : State
+    {
+        protected State Owner = null;
+
+        public BottomState(State _owner)
+        {
+            Owner = _owner;
+        }
+    }
+}
+
