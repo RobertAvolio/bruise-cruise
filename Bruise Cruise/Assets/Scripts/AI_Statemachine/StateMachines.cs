66a99d4137e059e3dc7f8f52d20871b14b3a4656
TheLoveDoctor <50722677+TheLoveDoctor@users.noreply.github.com>
2019-10-13 21:36:25 -0400

    (almost) finished statemachine implementation
    created a statemachinestuff namespace with a generic state class that can be used to assemble any kind of state machine, then used it to replicate marvin's AI statemachine
    
    still need to implement default substates and read some coding standards because everything is straight camel case

--- Bruise Cruise/Assets/Scripts/AI_Statemachine/StateMachines.cs
+++ Bruise Cruise/Assets/Scripts/AI_Statemachine/StateMachines.cs
@@ -1,46 +1,62 @@
-﻿using System.Collections.Generic;
+﻿using UnityEngine;
+using System.Collections.Generic;
 
+//all the stuff you would need to build a state, and then a state machine
+//TODO: figure out default substates, States should easily have them set when the statemachine is being assembled, and should run their enter function when the game starts
 namespace StateMachineStuff
 {
-    public class State
+    //Generic state class that all states and state machines inherit from
+    //<T> is what statemachine the state will be a part of
+    public abstract class State<T>
     {
-        public virtual void StateEnter() { }
-        public virtual string StateUpdate() => null;
-        public virtual void StateExit() { }
-    }
+        //functions to run when entering, exiting, and executing a state
+        public abstract void EnterState();
+        public abstract string RunState();
+        public abstract void ExitState();
 
-    public class MidState : State
-    {
+        //stores all of a states substates, along with their names
+        protected Dictionary<string, State<T>> SubStatesMap = new Dictionary<string, State<T>>();
 
-        protected IDictionary<string, State> SubStatesMap;
-
-        protected State CurrentSubState;
-
-        public void addSubState(string name, State state)
+        //stores the current state, should be protected but I haven't figured out default substates yet so I set it manually
+        public State<T> CurrentSubState = null;
+        
+        //the state all the way at the top of the tree, the 'Statemachine' that all it' states are a part of
+        protected T Owner;
+        
+        //sets the owner
+        public void setOwner(T owner)
         {
-            SubStatesMap.Add(new KeyValuePair<string, State>(name, state));
+            Owner = owner;
         }
-        protected void updateState(string NewState)
+
+        //appends a given substate and name to the substates map
+        public void addSubState(string name, State<T> state)
+        {
+            SubStatesMap.Add(name, state);
+        }
+
+        //Update function, only runs core state update code, state specific code goes into RunState()
+        public string StateUpdate()
+        {
+            if (CurrentSubState != null)
+            {
+                updateState(CurrentSubState.StateUpdate());
+            }
+            return this.RunState();
+        }
+
+        //Changes the substate, running the ExitState and EnterState functions for it
+        private void updateState(string NewState)
         {
             if (NewState != null)
             {
                 if (SubStatesMap[NewState] != CurrentSubState)
                 {
-                    CurrentSubState.StateExit();
+                    CurrentSubState.ExitState();
                     CurrentSubState = SubStatesMap[NewState];
-                    CurrentSubState.StateEnter();
+                    CurrentSubState.EnterState();
                 }
             }
-        }
-    }
-
-    public class BottomState : State
-    {
-        protected State Owner = null;
-
-        public BottomState(State _owner)
-        {
-            Owner = _owner;
         }
     }
 }
--- Bruise Cruise/Assets/Scripts/Enemy.cs
+++ Bruise Cruise/Assets/Scripts/Enemy.cs
@@ -7,10 +7,10 @@ public class Enemy : MonoBehaviour
     public int health = 5;
 
     // Start is called before the first frame update
-    void Start()
-    {
+    //void Start()
+    //{
         
-    }
+    //}
 
     // Update is called once per frame
     void Update()
