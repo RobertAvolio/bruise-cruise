556f473b00d8b48685caaa151066fae5281111cc
JLoiXP <justinhloi@yahoo.com>
2019-10-25 14:27:16 -0400

    BC-034-Start-Menu
    Start menu of game

---
+++ Bruise Cruise/Assets/ImagesFonts/printClick.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class printClick : MonoBehaviour
+{
+    public void print()
+    {
+        Debug.Log("Buttone clicked");
+    }
+}
