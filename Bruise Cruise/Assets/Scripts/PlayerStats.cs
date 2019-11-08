92473f3d566cba37e687ec22f2ff7ada5845bb20
TheLoveDoctor <50722677+TheLoveDoctor@users.noreply.github.com>
2019-11-08 13:57:57 -0500

    added enemy attacks
    Added a takeDamge() function to playerstats, not attached to player yet. script is called from the enemy's attack state

--- Bruise Cruise/Assets/Scripts/PlayerStats.cs
+++ Bruise Cruise/Assets/Scripts/PlayerStats.cs
@@ -2,9 +2,14 @@
 // using System.Collections.Generic;
 // using UnityEngine;
 
-public static class PlayerStats
+public class PlayerStats
 {
     private static int _playerHealth = 100;
+
+    public void TakeDamage()
+    {
+        _playerHealth -= 10;
+    }
 
     public static int PlayerHealth {
         get => _playerHealth;
