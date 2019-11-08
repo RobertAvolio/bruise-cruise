182b7c8bec9c01a3b06c9f3ccd1ed665f026246e
Robert Avolio <ravolio1@umbc.edu>
2019-11-07 23:22:19 -0500

    BC-038 New animations
    Falling animation still needs to be rotated properly. Added falling and jumping animations.

--- Bruise Cruise/Assets/Scripts/PlayerMovement.cs
+++ Bruise Cruise/Assets/Scripts/PlayerMovement.cs
@@ -12,6 +12,7 @@ public class PlayerMovement : MonoBehaviour
     private Rigidbody2D rb2d;
     private Vector2 moveVector;
     private bool jumping = false;
+    private bool falling = false;
     private Animator anim;
     private Transform tf;
     private float jumpTimer;
@@ -22,23 +23,33 @@ public class PlayerMovement : MonoBehaviour
         rb2d = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
         tf = GetComponent<Transform>();
-        //rb2d.gravityScale = 1f;
     }
 
     void Update() {
         
+        if (rb2d.velocity.y < 0)
+        {
+            anim.SetBool("Jumping", false);
+            anim.SetBool("Falling", true);
+        }
+        if(rb2d.velocity.y >= 0)
+        {
+            anim.SetBool("Falling", false);
+        }
+
         // Y movement
-        if (Input.GetKeyDown(KeyCode.Space) && rb2d.velocity.y == 0) { 
+        if ((Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown("joystick button 0")) && rb2d.velocity.y == 0) { 
             jumping = true;
+            anim.SetBool("Jumping", true);
             jumpTimer = jumpLength;
             rb2d.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * speed, jumpForce);
         }
-        if (Input.GetKeyUp(KeyCode.Space))
+        if ((Input.GetKeyUp(KeyCode.Space) || Input.GetKeyUp("joystick button 0")))
         {
             jumping = false;
         }
 
-        if (jumping && Input.GetKey(KeyCode.Space))
+        if (jumping && (Input.GetKey(KeyCode.Space) || Input.GetKey("joystick button 0")))
         {
 
             if (jumpTimer > 0)
