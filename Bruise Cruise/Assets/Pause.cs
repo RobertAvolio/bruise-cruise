07b16da10962fd2c7c7f4278ed153f0317238dad
Robert Avolio <ravolio1@umbc.edu>
2019-11-08 14:06:53 -0500

    BC-044 Playtest Ready
    Added pause feature. Added in the new ship. Added UI, still need to get the bottle working though.

---
+++ Bruise Cruise/Assets/Pause.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pause : MonoBehaviour
+{
+
+    [SerializeField] private GameObject pauseGUI;
+    // Start is called before the first frame update
+    void Start()
+    {
+        pauseGUI.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (!pauseGUI.activeInHierarchy)
+            {
+                //pause
+                Time.timeScale = 0;
+                pauseGUI.SetActive(true);
+            } else if (pauseGUI.activeInHierarchy)
+            {
+                //continue
+                Time.timeScale = 1;
+                pauseGUI.SetActive(false);
+            }
+        }
+    }
+}
