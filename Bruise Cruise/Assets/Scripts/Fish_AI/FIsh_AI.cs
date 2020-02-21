980e2fee6501f4c29609479c0ddf3796222d7677
TheLoveDoctor <50722677+TheLoveDoctor@users.noreply.github.com>
2020-02-21 13:51:27 -0500

    made eustace states not public because why were they public

---
+++ Bruise Cruise/Assets/Scripts/Fish_AI/FIsh_AI.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using StateMachineStuff;
+
+public class FIsh_AI : MonoBehaviour
+{
+    public enum actions
+    {
+        pos1,
+        pos2,
+        attack
+    }
+    [SerializeField]
+    public Vector3 position1, position2;
+    public List<actions> actionList;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
