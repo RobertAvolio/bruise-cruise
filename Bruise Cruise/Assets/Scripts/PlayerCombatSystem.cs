d1f78c0982675722de92fcfe041c6ebf1f71c95d
TheLoveDoctor <50722677+TheLoveDoctor@users.noreply.github.com>
2020-03-04 11:48:16 -0500

    updated playercombatsystem to work on fish and eustace
    temporary fix to playercombat, right now it just calls takedamage on eustaces ai component if it exists, then fish ai component if it exists. This will be temporary(LMAO no it won't) we need a general takesdamage component that chad's combat system can use which interfaces with the individual enemy scripts

--- Bruise Cruise/Assets/Scripts/PlayerCombatSystem.cs
+++ Bruise Cruise/Assets/Scripts/PlayerCombatSystem.cs
@@ -95,12 +95,14 @@ public class PlayerCombatSystem : MonoBehaviour
             foreach(Collider2D enemy in enemiesToDamage)
             {
                 enemy.GetComponent<Fish_AI>()?.TakeDamage();
+                enemy.GetComponent<Enemy_AI_Duel_StateMachine>()?.TakeDamage();
             }
         } else {
             Collider2D[] enemiesToDamage = Physics2D.OverlapBoxAll(new Vector2(L_Hitbox.position.x, L_Hitbox.position.y),new Vector2(3, 2), 0f);
             foreach(Collider2D enemy in enemiesToDamage)
             {
                 enemy.GetComponent<Fish_AI>()?.TakeDamage();
+                enemy.GetComponent<Enemy_AI_Duel_StateMachine>()?.TakeDamage();
             }
         }
     }
